1298fedf5562fa5dea8a00ecd77843dac687e652
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-12-22 14:59:05 +0100

    aggiunto oggetto db ad api_mdb, con parametri di connessione al db.

--- fintech_layer/db_fin/api_mdb.py
+++ fintech_layer/db_fin/api_mdb.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from database import Db
 
-db = Db("root","/Ferrari499p","")
+#Creating the object db from class Db, in ./database.py
+db = Db("root","/Ferrari499p","fintech-mariadb",3306,"fintech_db")
 
 app = FastAPI()
 
